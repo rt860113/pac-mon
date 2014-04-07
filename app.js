de691be7dc9121756610ccc5a49c7a1f688dc2b0
Tao Ren <rt860113@gmail.com>
2014-04-06 17:05:30 -0700

    Signed-off-by: Tao Ren <rt860113@gmail.com>

--- app.js
+++ app.js
@@ -3,34 +3,17 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
+var express = require('express.io');
 var path = require('path');
-
-var app = express();
+var app = express().http().io();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
+app.use(express.bodyParser());
+app.use(express.cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.session({secret:"Mylove"}));
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+var route=require("./routes/index")(app);
+app.listen(8000);
